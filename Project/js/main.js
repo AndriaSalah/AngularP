c27255e18a8ca4a36e32ade2e0f63bf918f91cbb
Andria Salah <74527457+AndriaSalah@users.noreply.github.com>
2022-08-11 00:43:05 +0200

    Add files via upload

---
+++ Project/js/main.js
@@ -0,0 +1,20 @@
+// let salary = prompt('please enter your salary')
+
+// let taxes = 0.1 * salary
+
+// console.log(salary - taxes)
+
+// //===================
+
+// let x = parseInt(prompt('please enter the first number'))
+// let y = parseInt(prompt('please enter the second number'))
+
+// console.log('sum is ' + (x+y))
+// console.log('subtraction is ' + (x-y))
+// console.log('multiplication is ' + (x*y))
+// if(y != 0){
+// console.log('Division is ' + (x/y))
+// }
+// else {
+//     console.log('cant divide by zero')
+// }
