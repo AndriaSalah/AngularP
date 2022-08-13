a553c798885363541600993fc61f3db05f285b0e
Andria Salah <74527457+AndriaSalah@users.noreply.github.com>
2022-08-13 02:45:21 +0200

    Add files via upload

---
+++ Background_Changer/scripts.js
@@ -0,0 +1,30 @@
+// let color1 = document.getElementById("color1")
+// let color2 = document.getElementById("color2")
+// let body = document.querySelector("body")
+
+// function changeGradient(){
+// body.style.background= `linear-gradient(to right , ${color1.value},${color2.value}`
+// }
+
+// color1.addEventListener('change',changeGradient)
+// color2.addEventListener('change',changeGradient)
+
+let color = document.querySelector('#color')
+let color1 = document.querySelector('#color1')
+let color2 = document.querySelector('#color2')
+let color3 = document.querySelector('#color3')
+let solid = document.querySelector('.solid')
+let solid1 = document.querySelector('.solid1')
+let linear = document.querySelector('.linear')
+
+console.log(solid.style)
+function changecolor(){
+    solid.style.background = color.value
+    solid1.style.background = color1.value
+    linear.style.background = `linear-gradient(to right , ${color2.value},${color3.value}`
+}
+
+color.addEventListener('change',changecolor)
+color1.addEventListener('change',changecolor)
+color2.addEventListener('change',changecolor)
+color3.addEventListener('change',changecolor)
